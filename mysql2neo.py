44d12dff9e0fb95ff805ec9fa62294e05b094c44
Ross Blair <rblair2@stanford.edu>
2017-04-20 16:43:21 -0700

    script used to move data straight from mysql to neo, not usable within docker at this moment

---
+++ mysql2neo.py
@@ -0,0 +1,107 @@
+from py2neo import Graph, Node, Relationship
+import pymysql
+
+conn = pymysql.connect(host='localhost', user='root', db='cogat')
+cursor = conn.cursor()
+
+graph = Graph("http://192.168.99.100:7474/db/data/")
+
+# import tasks
+#sql = "select * from table_term where term_type='task'"
+#cursor.execute(sql)
+#mysql_tasks = cursor.fetchall()
+
+# import indicators
+print("indicators...")
+sql = "select distinct indicator_text from type_indicator"
+cursor.execute(sql)
+indicators = cursor.fetchall()
+
+for indicator in indicators:
+    if graph.find_one("indicator", property_key="type", property_value=indicator[0]):
+        continue
+    gret = graph.create(Node("indicator", type=indicator[0]))
+    print(gret)
+    
+
+print("indicator relationships...")
+sql = "select * from type_indicator"
+cursor.execute(sql)
+indicators = cursor.fetchall()
+
+for indicator in indicators:
+    start_node = graph.find_one("task", property_key="id",
+                                property_value=indicator[2])
+    end_node = graph.find_one("indicator", property_key="type",
+                              property_value=indicator[3])
+
+    match = graph.match_one(start_node=start_node, rel_type="HASINDICATOR",
+                            end_node=end_node)
+    if start_node and end_node and not match:
+        gret = graph.create(Relationship(start_node, "HASINDICATOR", end_node,
+                                          id=indicator[0], id_user=indicator[1],
+                                          event_stamp=indicator[4]))
+        print(gret)
+
+
+# import external datasets
+print("external datasets...")
+sql = "select * from external_datasets"
+cursor.execute(sql)
+external_datasets = cursor.fetchall()
+
+for external_dataset in external_datasets:
+    found = graph.find_one("external_dataset", property_key="id",
+                           property_value=external_dataset[0])
+    if not found:
+        gret = graph.create(
+            Node("external_dataset", id=external_dataset[0],
+                 id_term=external_dataset[1], dataset_name=external_dataset[2],
+                 dataset_uri=external_dataset[3], id_user=external_dataset[4],
+                 event_stamp=external_dataset[5])
+        )
+        print(gret)
+
+    start_node = graph.find_one("task", property_key="id",
+                                property_value=external_dataset[1])
+    end_node = graph.find_one("external_dataset", property_key="id",
+                              property_value=external_dataset[0])
+
+    match = graph.match_one(start_node=start_node, rel_type="HASEXTERNALDATASET",
+                            end_node=end_node)
+    if start_node and end_node and not match:
+        gret = graph.create(Relationship(start_node, "HASEXTERNALDATASET",
+                                          end_node))
+        print(gret)
+
+# import implementations
+print("implementations...")
+sql = "select * from table_implementation"
+cursor.execute(sql)
+implementations = cursor.fetchall()
+
+for implementation in implementations:
+    found = graph.find_one("implementation", property_key="id", property_value=implementation[0])
+    if not found:
+        gret = graph.create(Node("implementation", id=implementation[0],
+                                 id_task=implementation[1],
+                                 implementation_name=implementation[2],
+                                 implementation_uri=implementation[3],
+                                 id_user=implementation[4],
+                                 event_stamp=implementation[5],
+                                 implementation_description=implementation[5]))
+        print(gret)
+
+    start_node = graph.find_one("task", property_key="id",
+                                property_value=implementation[1])
+    end_node = graph.find_one("implementation", property_key="id",
+                              property_value=implementation[0])
+
+    match = graph.match_one(start_node=start_node, rel_type="HASIMPLEMENTATION",
+                            end_node=end_node)
+
+    if start_node and end_node and not match:
+        gret = graph.create(Relationship(start_node, "HASIMPLEMENTATION",
+                            end_node))
+        print(gret)
+

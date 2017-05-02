e77baa1e4c66253385adb46672c3c61882548740
Ross Blair <rblair2@stanford.edu>
2017-05-02 12:58:36 -0700

    only print output if something missing or mismatched was found.

--- scripts/api_compare.py
+++ scripts/api_compare.py
@@ -2,7 +2,7 @@
     the new one '''
 import json
 
-from cognitiveatlas.api import get_task, get_concept
+from cognitiveatlas.api import get_task, get_concept, get_disorder
 from cognitiveatlas.utils import get_json
 
 BASE_EP = "http://192.168.99.100/api/"
@@ -25,6 +25,15 @@ def concept_json_dump():
     with open("all_concepts.json", 'w') as fp:
         json.dump(all_concepts, fp)
 
+def disorder_json_dump():
+    ''' use cognitiveatlas library to dump disorders to a json file '''
+    all_disorders = []
+    disorders = get_disorder().json
+    for disorder in disorders:
+        all_disorders.append(get_disorder(id=disorder['id']).json)
+    with open("all_disorders.json", 'w') as fp:
+        json.dump(all_disorders, fp)
+
 def dict_in_list(old_dict, new_list):
     ''' See if a given dictionary from the old api appears in a list of
         dictionaries from the new api.'''
@@ -39,7 +48,7 @@ def compare_dicts(old_dict, new_dict, ignore_keys=[]):
     missing_keys = {}
     mismatched_value = []
     mismatched_types = []
-    old_dict = old_dict[0]
+    old_dict = old_dict
     for old_key in old_dict:
         if old_key in ignore_keys:
             continue
@@ -47,7 +56,8 @@ def compare_dicts(old_dict, new_dict, ignore_keys=[]):
             old_value = old_dict[old_key]
             new_value = new_dict[old_key]
             if type(old_value).__name__ == 'list' and type(new_value).__name__ == 'list':
-                pass
+                if len(old_value) != len(new_value):
+                    mismatched_value.append((old_dict['id'], old_key, len(old_value), len(new_value)))
             elif type(old_value).__name__ == 'str' and type(new_value).__name__ == 'str':
                 if old_value != new_value:
                     mismatched_value.append((old_dict['id'], old_key, old_value, new_value))
@@ -58,9 +68,12 @@ def compare_dicts(old_dict, new_dict, ignore_keys=[]):
                 missing_keys[error.args[0]].append(new_dict['id'])
             else:
                 missing_keys[error.args[0]] = [new_dict['id']]
-    print(missing_keys)
-    print(mismatched_value)
-    print(mismatched_types)
+    if missing_keys:
+        print(missing_keys)
+    if mismatched_value:
+        print(mismatched_value)
+    if mismatched_types:
+        print(mismatched_types)
 
 def compare_api_elems(file, url, ignore_keys=[]):
     ''' iterate over a  json file and attempt to lookup each element at
@@ -71,12 +84,17 @@ def compare_api_elems(file, url, ignore_keys=[]):
 
     for old_elem in old_elems:
         new_elem = json.loads(get_json("{}?id={}".format(url, old_elem[0]['id'])))
-        compare_dicts(old_elem, new_elem, ignore_keys)
+        compare_dicts(old_elem[0], new_elem, ignore_keys)
 
 if  __name__ == '__main__':
     #task_json_dump()
-    ignore_keys = ['concept_class', 'umark', 'umarkdef', 'conclass', 'event_stamp', 'def_event_stamp', 'discussion', 'history']
-    compare_api_elems("all_tasks.json", "{}{}".format(BASE_EP, "task"), ignore_keys)
+    #ignore_keys = ['concept_class', 'umark', 'umarkdef', 'conclass', 'event_stamp', 'def_event_stamp', 'discussion', 'history']
+    #compare_api_elems("all_tasks.json", "{}{}".format(BASE_EP, "task"), ignore_keys)
 
     #concept_json_dump()
-    #compare_api_elems("all_concepts.json", "{}{}".format(BASE_EP, "concept"))
+    #ignore_keys = ['concept_class', 'def_event_stamp', 'event_stamp', 'definition_text']
+    #compare_api_elems("all_concepts.json", "{}{}".format(BASE_EP, "concept"), ignore_keys)
+
+    #disorder_json_dump()
+    ignore_keys = ['tid', 'type']
+    compare_api_elems("all_disorders.json", "{}{}".format(BASE_EP, "disorder"))

caa75aa42e2d66d02383e6660923b25234d2a6d0
Ross Blair <rblair2@stanford.edu>
2017-05-03 16:17:27 -0700

    reorganize docker-compose files

--- cognitive/bogus_secrets.py
+++
@@ -1,7 +0,0 @@
-TURK = {
-    'host': 'mturk.com/mturk/externalSubmit',
-    'sandbox_host':'workersandbox.mturk.com/mturk/externalSubmit',
-    'app_url':'expfactory.org',
-    'debug': 1
-}
-SECRET_KEY='bogus'

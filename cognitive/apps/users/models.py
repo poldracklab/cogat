5471962bd92cdd0b853cc8102a52fca42d234b19
Ross Blair <rblair2@stanford.edu>
2017-05-08 16:37:58 -0700

    reorganize graphdb files. Start work on import containers entrypoint

--- cognitive/apps/users/models.py
+++ cognitive/apps/users/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class User(AbstractUser):
     obfuscate = models.BooleanField(default=False)
-    # 512 was what it is in old db
-    interest_tags = models.CharField(max_length=512)
-    org_id = models.CharField(max_length=36)
-    old_id = models.CharField(max_length=36)
-
+    interest_tags = models.TextField(blank=True)
+    specialist_tags = models.TextField(blank=True)
+    org_id = models.CharField(max_length=36, blank=True)
+    old_id = models.CharField(max_length=36, blank=True)
+    title = models.Charfield(max_length=255, blank=True)
--- compose/import/entrypoint.sh
+++ compose/import/entrypoint.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
+# assuming initial django migrations for postgres have already been run
+
 
 cd /code
 sleep 15
 python3 scripts/migrate_database.py
 
+if [ ! -z "$MYSQL_DUMP" ]; then
+    if [ -f "$MYSQL_DUMP" ]; then
+        mysql_imports
+    else
+        echo "$MYSQL_DUMP is not an extant or readable file"
+    fi
+else
+    echo "Variable MYSQL_DUMP unset or null"
+fi
+
+mysql_imports ()
+{
+    # entrypoint from mysql docker image.
+    /entrypoint.sh mysqld &
+    sleep 8
+    echo "CREATE DATABASE cogat;" | mysql -u root
+    mysql -u root < $MYSQL_DUMP
+    python /code/scripts/mysql2neo.py
+    python /code/scripts/user_import.py
+}

203b38bcf7f19811822b9a855b84ebab7b64996c
Ross Blair <rblair2@stanford.edu>
2017-05-09 17:12:51 -0700

    register our custom user model with admin pages in django.

---
+++ cognitive/apps/users/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm
+
+from cognitive.apps.users.models import User
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('obfuscate', 'interest_tags', 'specialist_tags',
+                           'old_id', 'org_id', 'title', 'rank')}),
+    )
+
+admin.site.register(User, CustomUserAdmin)

0c5e40449924fd67de212b5035d78f7ee02773ef
Ross Blair <rblair2@stanford.edu>
2017-11-28 15:15:07 -0800

    have counts update on every load for now

--- cognitive/apps/main/context_processors.py
+++ cognitive/apps/main/context_processors.py
@@ -1,21 +1,20 @@
-from cognitive.apps.atlas import query
-
-Concept = query.Concept()
-Task = query.Task()
-Disorder = query.Disorder()
-Theory = query.Theory()
-Battery = query.Battery()
-Behavior = query.Behavior()
-Trait = query.Trait()
-
-# Needed on all pages
-counts = {
-    "disorders": Disorder.count(),
-    "tasks": Task.count(),
-    "concepts": Concept.count(),
-    "collections": Battery.count() + Theory.count(),
-    "phenotypes": (Disorder.count() + Trait.count() + Behavior.count()),
-}
-
 def counts_processor(request):
+    import cognitive.apps.atlas.query as query
+
+    Concept = query.Concept()
+    Task = query.Task()
+    Disorder = query.Disorder()
+    Theory = query.Theory()
+    Battery = query.Battery()
+    Behavior = query.Behavior()
+    Trait = query.Trait()
+
+    counts = {
+        "disorders": Disorder.count(),
+        "tasks": Task.count(),
+        "concepts": Concept.count(),
+        "collections": Battery.count() + Theory.count(),
+        "phenotypes": (Disorder.count() + Trait.count() + Behavior.count()),
+    }
+
     return {'counts': counts}

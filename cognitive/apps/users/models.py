3dab37e0767f15fa9ec21caeb357b8cdec4ab05b
Ross Blair <rblair2@stanford.edu>
2017-12-06 20:23:13 -0800

    send out email on new user creation

--- cognitive/apps/users/models.py
+++ cognitive/apps/users/models.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.core.mail import send_mail
 from django.core.urlresolvers import reverse
 from django.db import models
 
@@ -10,3 +12,24 @@ class User(AbstractUser):
     org_id = models.CharField(max_length=36, blank=True, null=True)
     title = models.CharField(max_length=255, blank=True)
     rank = models.CharField(max_length=10, default="0")
+
+    def save(self, *args, **kwargs):
+        notify = False
+        if self.pk is None:
+            notify = True
+
+        super(User, self).save(*args, **kwargs)
+
+        if notify:
+            new_user_notify(self)
+
+def new_user_notify(user):
+    subject = "New User Registered for Cognitive Atlas"
+    body = '''
+    <p>Email: {}</p>
+    <p>First Name: {}</p>
+    <p>Last Name: {}</p>
+    <p>Interests: {}</p>
+    '''.format(user.email, user.first_name, user.last_name, user.interest_tags)
+    send_mail(subject, "", "newuser@openfmri.org",
+              settings.NOTIFY_EMAILS, html_message=body)
